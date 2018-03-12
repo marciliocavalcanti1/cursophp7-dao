3a3088b8ebce358120ae80cce21af1a436d9e0de
Marcilio <marcilio_surf@hotmail.com>
2018-03-12 00:33:59 -0300

    Delete command added

--- index.php
+++ index.php
@@ -32,12 +32,18 @@ require_once("config.php");
 //echo $aluno;
 
 # AULA S13A66: PDO - DAO - UPDATE
+//$usuario = new Usuario();
+//$usuario->loadById(13);
+//$usuario->update("professor", "!@#$%¨&*");
+//echo $usuario;
+
 $usuario = new Usuario();
 
 $usuario->loadById(13);
 
-$usuario->update("professor", "!@#$%¨&*");
+$usuario->delete();
 
 echo $usuario;
+
 
  ?>
